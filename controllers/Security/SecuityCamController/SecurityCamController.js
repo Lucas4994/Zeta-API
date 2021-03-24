d929ad36ba614eda0ace77c8d21356dc223ce6ea
Lucas4994 <lucasgomes4994@gmail.com>
2021-03-23 22:16:40 -0300

    fix security cam route

--- controllers/Security/SecuityCamController/SecurityCamController.js
+++ controllers/Security/SecuityCamController/SecurityCamController.js
@@ -1,7 +1,7 @@
 const getSecurityCamVideo = (req, res) => {
     const {frame} = req.body; 
 	res.writeHead(200, {'Content-Type': 'multipart/x-mixed-replace; boundary=frame'});
-    res.end();
+    res.end(frame);
 }
 
 module.exports = {getSecurityCamVideo}
